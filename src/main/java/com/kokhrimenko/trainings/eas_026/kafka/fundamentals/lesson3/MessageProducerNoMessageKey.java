76d8024adeb2f2c7481b35049a9f6ddca8990eb6
Kostiantyn Okhrimenko <konstantin.okhrimenko@asml.com>
2022-04-07 15:53:13 +0200

    add more kafka producers implementations

--- src/main/java/com/kokhrimenko/trainings/eas_026/kafka/fundamentals/lesson3/MessageProducerNoMessageKey.java
+++ src/main/java/com/kokhrimenko/trainings/eas_026/kafka/fundamentals/lesson3/MessageProducerNoMessageKey.java
@@ -10,13 +10,13 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Simple example: how to send a message to a kafka cluster.
+ * Send message to a kafka cluster, without explicit message key and check kafka round-robin partitioning strategy.
  *
  * @author kokhrime
  *
  */
 @Slf4j
-public class SimpleMessageProducer {
+public class MessageProducerNoMessageKey {
 	private static final String KAFKA_PROPERTIES = "kafka.properties";
 	private static final String KAFKA_TOPIC = "SimpleMessageProducertopic";
 
@@ -25,9 +25,13 @@ public class SimpleMessageProducer {
 		Properties prop = new Properties();
 		prop.load(SimpleMessageProducer.class.getResourceAsStream(KAFKA_PROPERTIES));
 
+		final int countOfIterations = 6;
 		try (Producer<String, String> producer = new KafkaProducer<>(prop)) {
-			producer.send(new ProducerRecord<>(KAFKA_TOPIC, "msg.key", "hello world"));
-			log.info("Message was successfully sent");
+			for (int i = 0; i < countOfIterations; i++) {
+				producer.send(new ProducerRecord<>(KAFKA_TOPIC, String.format("Message without message key %d: hello world", i)));
+				log.info("Message {} was successfully sent", i);
+				producer.flush();
+			}
 		}
 		log.info("End of SimpleMessageProducer");
 	}
--- src/main/java/com/kokhrimenko/trainings/eas_026/kafka/fundamentals/lesson3/solution/MessageProducerWithExplicitPartition.java
+++ src/main/java/com/kokhrimenko/trainings/eas_026/kafka/fundamentals/lesson3/solution/MessageProducerWithExplicitPartition.java
@@ -1,4 +1,4 @@
-package com.kokhrimenko.trainings.eas_026.kafka.fundamentals.lesson3;
+package com.kokhrimenko.trainings.eas_026.kafka.fundamentals.lesson3.solution;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -7,18 +7,21 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import com.kokhrimenko.trainings.eas_026.kafka.fundamentals.lesson3.SimpleMessageProducer;
+
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Simple example: how to send a message to a kafka cluster.
+ * Example: how to send a message to a kafka cluster and explicitly provide a partition.
  *
  * @author kokhrime
  *
  */
 @Slf4j
-public class SimpleMessageProducer {
+public class MessageProducerWithExplicitPartition {
 	private static final String KAFKA_PROPERTIES = "kafka.properties";
 	private static final String KAFKA_TOPIC = "SimpleMessageProducertopic";
+	private static final int KAFKA_PARTITION = 2;
 
 	public static void main(String[] args) throws IOException {
 		log.info("Start of SimpleMessageProducer");
@@ -26,7 +29,7 @@ public class SimpleMessageProducer {
 		prop.load(SimpleMessageProducer.class.getResourceAsStream(KAFKA_PROPERTIES));
 
 		try (Producer<String, String> producer = new KafkaProducer<>(prop)) {
-			producer.send(new ProducerRecord<>(KAFKA_TOPIC, "msg.key", "hello world"));
+			producer.send(new ProducerRecord<>(KAFKA_TOPIC, KAFKA_PARTITION, "msg.key_1", "hello world from partition: " + KAFKA_PARTITION));
 			log.info("Message was successfully sent");
 		}
 		log.info("End of SimpleMessageProducer");
