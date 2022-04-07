ea97ccd104eb0599c1fdf66b1aff72a889690b31
Kostiantyn Okhrimenko <konstantin.okhrimenko@asml.com>
2022-04-07 11:19:43 +0200

    add simple kafka producer

--- src/main/java/com/kokhrimenko/trainings/eas_026/kafka/fundamentals/lesson3/SimpleMessageProducer.java
+++ src/main/java/com/kokhrimenko/trainings/eas_026/kafka/fundamentals/lesson3/SimpleMessageProducer.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,10 +18,11 @@ public class SimpleMessageProducer {
 		log.info("Start of SimpleMessageProducer");
 		Properties prop = new Properties();
 		prop.load(SimpleMessageProducer.class.getResourceAsStream(KAFKA_PROPERTIES));
-		
-		System.out.println(prop);
-		Producer<String, String> producer = new KafkaProducer<>(prop);
-		
+
+		try (Producer<String, String> producer = new KafkaProducer<>(prop)) {
+			producer.send(new ProducerRecord<>(KAFKA_TOPIC, "msg.key", "hello world"));
+			log.info("Message was successfully sent");
+		}
 		log.info("End of SimpleMessageProducer");
 	}
 	
